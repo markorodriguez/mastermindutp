ed9192535540057ae7a45bf0d2c020eebf2fb59d
Marko <marko.rodriguez@hotmail.es>
2020-11-11 01:18:53 -0500

    test

--- src/components/Juego.jsx
+++ src/components/Juego.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react"
-
 import Row from "./Row"
 
 export default function Game() {
@@ -8,8 +7,10 @@ export default function Game() {
     const [answers, setAnswers] = useState([])  //arreglos realizados
     const [secret, setSecret] = useState([]) //combinación a adivinar
     const [disabled, setDisabled] = useState(true)
+    //               0            1       2       3       4           5
     const colors = ['green', 'purple', 'red', 'yellow', 'blue', 'brown']
     const [counter, setCounter] = useState(0)
+    //
 
     const addCount = (evt) => {
         let savedArray = [...guesses];
@@ -24,14 +25,16 @@ export default function Game() {
             setGuesses(savedArray)
         }
     }
-
-    const saveArray = (evt) => {
+    
+    const saveArray = () => {
         setAnswers((prevState) => { return [...prevState, guesses] })
         setCounter(counter + 1)
         if (counter >= 9) {
             setDisabled(true)
             document.getElementById('resetBtn').style.visibility = ""
         }
+
+        console.log(secret)
     }
 
     const generateColors = () => {
@@ -47,43 +50,68 @@ export default function Game() {
 
             }
             setSecret(code)
-            console.log(secret)
             setDisabled(false)
+
             alert(username + ' el juego ha empezado')
-           document.getElementById('btnStart').style.visibility = "hidden"
+            document.getElementById('btnStart').style.visibility = "hidden"
         }
+
     };
 
     const resetGame = () => {
-        setGuesses([0, 0, 0, 0]) //alternativas
-        setAnswers([])  //arreglos realizados
-        setSecret([]) //combinación a adivinar
+        setGuesses([0, 0, 0, 0])
+        setAnswers([])
+        setSecret([])
         setCounter(0)
 
         generateColors();
     }
 
+    /*
+    const compareArrays = (arrayToCheck) => {
+        const array1 = arrayToCheck
+        const array2 = secret
+
+        if (array1 === array2) {
+            console.log('ganaste')
+        } else {
+            console.log(array1)
+            
+            array1.map((el, index) => {
+                for (let i = 0; i <= 3; i++) {
+                    if (el === array2[i]) {
+                        setCorrectas(correctas + 1)
+                        if (el === array2[index]) {
+                            setPosiciones(posicionesCorrectas + 1)
+                        }
+                    }
+                }
+                return [correctas, posicionesCorrectas]
+            }
+            )
+        }
+    }
+    */
+
     const styleOption = "butn btn-rounded"
+
+
 
     return (
         <div className="container-fluid" style={{ textAlign: 'center' }}>
-            <button className="btn" onClick={generateColors} id="btnStart">START GAME</button> <br />
-            <button className="btn" onClick={resetGame} id="resetBtn" style={{ visibility: "hidden" }}>RESET GAME</button> <br />
-            <button value="0" onClick={addCount} disabled={disabled} className={styleOption + " " + colors[guesses[0]]}>1</button>
-            <button value="1" onClick={addCount} disabled={disabled} className={styleOption + " " + colors[guesses[1]]}>2</button>
-            <button value="2" onClick={addCount} disabled={disabled} className={styleOption + " " + colors[guesses[2]]}>3</button>
-            <button value="3" onClick={addCount} disabled={disabled} className={styleOption + " " + colors[guesses[3]]}>4</button>
-            <button className="btn" onClick={saveArray} disabled={disabled} id="validate">Validate</button>
+            <div>
+                <button className="btn" onClick={generateColors} id="btnStart">START GAME</button> <br />
+                <button className="btn" onClick={resetGame} id="resetBtn" style={{ visibility: "hidden" }}>RESET GAME</button> <br />
+                <button value="0" onClick={addCount} disabled={disabled} className={styleOption + " " + colors[guesses[0]]}>1</button>
+                <button value="1" onClick={addCount} disabled={disabled} className={styleOption + " " + colors[guesses[1]]}>2</button>
+                <button value="2" onClick={addCount} disabled={disabled} className={styleOption + " " + colors[guesses[2]]}>3</button>
+                <button value="3" onClick={addCount} disabled={disabled} className={styleOption + " " + colors[guesses[3]]}>4</button>
+                <button className="btn" onClick={saveArray} disabled={disabled} id="validate">Agregar</button>
 
-
-
-            <div style={{ marginTop: '30px' }}>
-                <p>{answers.length}</p>
-                {answers.map((el, index) => <Row key={index} arreglo={el} count={counter} />)}
+                <div style={{ marginTop: '10px' }}>
+                    {answers.map((el, index) => <Row key={index} arreglo={el} count={counter}  secretArray={secret} />)} 
+                </div>
             </div>
-
         </div>
-
-
     )
 }
--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,14 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "./Button";
 
 export default function Row(props) {
     const colors = ['green', 'purple', 'red', 'yellow', 'blue', 'brown']
-    const countRow = props.count;
+    const [corr, setCorr] = useState(0)
+    const [posi, setPosi] = useState(0)
+    const [inco, setInco] = useState(0)
 
+    const validate = (evt) => {
+
+        const array1 = props.arreglo
+        const array2 = props.secretArray
+        let correctas = 0
+        let posicionesCorrectas = 0
+
+        if (array1 === array2) {
+            console.log('ganaste')
+        } else {
+            // eslint-disable-next-line array-callback-return
+            array1.map((el, index) => {
+                for (let i = 0; i <= 3; i++) {
+                    //posiblemente requiera cambiar la estructura
+                    if (el === array2[i]) {
+                        correctas = correctas + 1
+                        setCorr(correctas)
+                        if (el === array2[index]) {
+                            posicionesCorrectas = posicionesCorrectas + 1
+                            setPosi(posicionesCorrectas)
+                        }
+                    }
+                }
+            }
+            )
+            let incorrectas = 4 - correctas
+            console.log('las correctas son : ' + correctas)
+            console.log('las incorrectas son : ' + incorrectas)
+            console.log('las posiciones correctas son: ' + posicionesCorrectas)
+        }
+
+        setInco(4 - corr)
+
+        if (inco <= 0) {
+            evt.target.style.visibility = "hidden"
+        }
+    }
 
     return (
         <div>
-            {countRow<=10 ? props.arreglo.map((el,index)=><Button key={index} color={colors[el]} />) : props.funcion}
+            { props.arreglo.map((el, index) => <Button key={index} color={colors[el]} />)}
+            <button onClick={validate}>Validar</button>
+            <p>correctas : {corr}</p>
+            <p>correctas y buena posicion :{posi}</p>
+            <p>incorrectas : {inco}</p>
         </div>
     );
 }
