a7e2e42123a33cf9a523763bc1e9014711e3c061
Marko <marko.rodriguez@hotmail.es>
2020-11-11 12:47:31 -0500

    css and validation has been fixed

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -3,55 +3,43 @@ import Button from "./Button";
 
 export default function Row(props) {
     const colors = ['green', 'purple', 'red', 'yellow', 'blue', 'brown']
+    const _ = require('lodash')
+
     const [corr, setCorr] = useState(0)
     const [posi, setPosi] = useState(0)
-    const [inco, setInco] = useState(0)
 
+    let acumulador = 0
+    let exactos = 0
+    const arr = props.arreglo
+    const brr = props.secretArray
     const validate = (evt) => {
 
-        const array1 = props.arreglo
-        const array2 = props.secretArray
-        let correctas = 0
-        let posicionesCorrectas = 0
 
-        if (array1 === array2) {
-            console.log('ganaste')
-        } else {
-            // eslint-disable-next-line array-callback-return
-            array1.map((el, index) => {
-                for (let i = 0; i <= 3; i++) {
-                    //posiblemente requiera cambiar la estructura
-                    if (el === array2[i]) {
-                        correctas = correctas + 1
-                        setCorr(correctas)
-                        if (el === array2[index]) {
-                            posicionesCorrectas = posicionesCorrectas + 1
-                            setPosi(posicionesCorrectas)
-                        }
-                    }
+        for (let i = 0; i <= 3; i++) {
+            const booleanValue = _.includes(brr, arr[i])
+            if (booleanValue === true) {
+                acumulador = acumulador + 1
+                if (arr[i] === brr[i]) {
+                    exactos = exactos + 1
                 }
             }
-            )
-            let incorrectas = 4 - correctas
-            console.log('las correctas son : ' + correctas)
-            console.log('las incorrectas son : ' + incorrectas)
-            console.log('las posiciones correctas son: ' + posicionesCorrectas)
         }
+        setCorr(acumulador)
+        setPosi(exactos)
+        console.log('existen ' + acumulador)
+        console.log('existen exactos ' + exactos)
 
-        setInco(4 - corr)
-
-        if (inco <= 0) {
-            evt.target.style.visibility = "hidden"
-        }
+        evt.target.disabled = true
     }
 
     return (
-        <div>
+        <div className="respuestas">
             { props.arreglo.map((el, index) => <Button key={index} color={colors[el]} />)}
-            <button onClick={validate}>Validar</button>
-            <p>correctas : {corr}</p>
-            <p>correctas y buena posicion :{posi}</p>
-            <p>incorrectas : {inco}</p>
+            <button className="handleValidate" onClick={validate}>Validar</button>
+            <div className="btn-container">
+                <button className="validar" disabled style={{backgroundColor:'red'}}>{corr }</button>
+                <button className="validar" disabled style={{backgroundColor:'black', color: '#decdc3'}} >{posi }</button>
+            </div>
         </div>
     );
 }
