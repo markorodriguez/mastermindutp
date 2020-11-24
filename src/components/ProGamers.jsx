a0db60b76b4325c6fbc74dd26192adcd9b9f6f81
Marko <marko.rodriguez@hotmail.es>
2020-11-23 22:00:08 -0500

    fixed show ranking interface

--- src/components/ProGamers.jsx
+++ src/components/ProGamers.jsx
@@ -5,11 +5,11 @@ function ProGamers(props) {
         <div className="container">
             <div className="row">
                 <div className="col-6">
-                    <h4>Nickname</h4>
+                    
                     <h2>{props.username}</h2>
                 </div>
                 <div className="col-6">
-                    <h4>Intentos</h4>
+                    
                     <h2>{props.score}</h2>
                 </div>
             </div>
--- src/components/Ranking.jsx
+++ src/components/Ranking.jsx
@@ -10,11 +10,22 @@ function Ranking() {
     }
 
     return(
-        <div className="container" style={{textAlign: 'center', marginTop: '5%'}}>
+        <div className="container" style={{textAlign: 'center', marginTop: '5%', color:'black'}}>
             <h1>Ranking de jugadores</h1>
             <button className="btn btn-dark" style={{marginTop: '20px'}} onClick={getRanking}>Mostrar Ranking</button>
 
             <div style={{marginTop: '50px'}}>
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <h4>Usuario</h4>
+                    </div>
+                    <div className="col">
+                        <h4>Intentos</h4>
+                    </div>
+                </div>
+            </div>
+            
             {data.map((el,index)=><ProGamers key={index} username={el.username} score={el.score} /> )}
             </div>
       
